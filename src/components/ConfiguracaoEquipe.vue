fde6ea3bed0755917f519efed42b6d3bdb81b3a4
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-12-20 15:56:00 -0300

    aula 337 - mapState()

--- src/components/ConfiguracaoEquipe.vue
+++ src/components/ConfiguracaoEquipe.vue
@@ -7,31 +7,42 @@
     </div>
     <div class="row">
       <div class="col-8">
-        <p>Enfermeiro:</p>
-        <p>Socorrista:</p>
-        <p>Médico:</p>
-        <p>Carro:</p>
-        <p>Telefone:</p>
-        <p>Kit de reanimação:</p>
+        <!-- <p>Enfermeiro: {{ $store.state.equipe.enfermeiro }}</p>
+        <p>Socorrista: {{ $store.state.equipe.socorrista }}</p>
+        <p>Médico: {{ $store.state.equipe.medico }}</p>
+        <p>Carro: {{ $store.state.equipe.carro }}</p>
+        <p>Telefone: {{ $store.state.equipe.telefone }}</p>
+        <p>Kit de reanimação: {{ $store.state.equipe.kitDeReanimacao }}</p> -->
+        <p>Enfermeiro: {{ equipe.enfermeiro }}</p>
+        <p>Socorrista: {{ equipe.socorrista }}</p>
+        <p>Médico: {{ equipe.medico }}</p>
+        <p>Carro: {{ equipe.carro }}</p>
+        <p>Telefone: {{ equipe.telefone }}</p>
+        <p>Kit de reanimação: {{ equipe.kitDeReanimacao }}</p>
       </div>
       <div class="col-4 text-center">
         <div class="row">
           <div class="col">
-            <img class="img-fluid" :src="require('@/assets/ambulancias/indefinida.png')">
+            <img
+              class="img-fluid"
+              :src="require('@/assets/ambulancias/indefinida.png')"
+            />
           </div>
         </div>
         <div class="row mt-3">
           <div class="col">
             <button type="button" class="btn btn-primary">Montar equipe</button>
           </div>
-        </div>              
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-    name: 'ConfiguracaoEquipe'
-}
+  name: "ConfiguracaoEquipe",
+  computed: mapState(["equipe"]),
+};
 </script>
