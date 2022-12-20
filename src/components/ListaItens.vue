28c9b41656dbc10eb7ea8e014138fa2a62aa13b5
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-12-20 17:19:36 -0300

    aula 342 - Listando

--- src/components/ListaItens.vue
+++ src/components/ListaItens.vue
@@ -1,21 +1,53 @@
 <template>
-    <div>
-        Lista Itens {{ tipo }}
-        <br>
-        <item />
-    </div>
+  <div>
+    Lista Itens {{ tipo }}
+    <hr />
+    {{ itens }}
+    <item />
+  </div>
 </template>
 
 <script>
-import Item from '@/components/Item.vue'
+import Item from "@/components/Item.vue";
+import { mapState } from "vuex";
 
 export default {
-    name: 'ListaItens',
-    components: { 
-        Item
+  name: "ListaItens",
+  components: {
+    Item,
+  },
+  props: {
+    tipo: String,
+  },
+  computed: {
+    /*
+      enfermeiros() {
+      return this.$store.state.enfermeiros;
     },
-    props: {
-        tipo: String
-    }
-}
+    socorristas() {
+      return this.$store.state.socorristas;
+    },
+    medicos() {
+      return this.$store.state.medicos;
+    },
+    */
+
+    ...mapState({
+      enfermeiros: (state) => state.enfermeiros,
+      socorristas: (state) => state.socorristas,
+      medicos: (state) => state.medicos,
+    }),
+    itens() {
+      switch (this.tipo) {
+        case "enfermeiros":
+          return this.enfermeiros;
+        case "socorristas":
+          return this.socorristas;
+        case "medicos":
+          return this.medicos;
+      }
+      return [];
+    },
+  },
+};
 </script>
